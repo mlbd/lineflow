b8f12128a98e99ca07e3c993aedd87b9004beb1d
nildoria <sabbir.ahmed87@gmail.com>
2025-10-12 01:23:11 +0600

    Test Commit

--- src/components/page/TopBar.jsx
+++ src/components/page/TopBar.jsx
@@ -73,7 +73,7 @@ export default function TopBar({ onCartClick }) {
           </Link>
         </div>
 
-        {/* Cart Icon */}
+        {/*Cart Icon*/}
         {/* <div className="flex items-center">
           <button
             className="relative group cursor-pointer ml-5"
